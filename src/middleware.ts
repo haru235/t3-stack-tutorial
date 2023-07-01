c3f023a3f5643106de10c9bfdf1689b3455a97cd
HaruSakai <harusakai0515@gmail.com>
2023-07-01 02:07:02 -0500

    Fixed?

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,11 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-export default authMiddleware();
+export default withClerkMiddleware( () =>
+{
+  return NextResponse.next();
+} );
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
